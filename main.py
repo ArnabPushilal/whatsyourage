c136dcb647ff98c0b9106d7ae9af6d0c50d23403
Arnab Pushilal <arnab.pushilal.21@ucl.ac.uk>
2022-05-31 12:03:17 +0100

    updated for deployment

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 
 from app import create_app
-
+import os
+#from waitress import serve
+from flask import Flask
 
 app=create_app()
 
 
 if __name__=="__main__":
 
-    app.run(debug=True)
+    app.run()
+   
+    
+   
+   
